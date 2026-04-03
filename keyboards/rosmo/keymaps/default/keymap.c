0045920a8742862d924959e0fda74cd1fb31f41c
Taneli Leppä <rosmo@rosmo.fi>
2026-04-03 14:47:29 +0200

    Update keyboard files

--- keyboards/rosmo/keymaps/default/keymap.c
+++ keyboards/rosmo/keymaps/default/keymap.c
@@ -16,14 +16,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "users/holykeebs/holykeebs.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 LAYOUT(
-    KC_A),
+    QK_MOUSE_BUTTON_1, HK_D_MODE, QK_MOUSE_BUTTON_2,
+    MO(1), MO(2), KC_WWW_BACK),
+
+LAYOUT(
+    HK_SAVE, HK_I_SCROLL, HK_C_SCROLL,
+    QK_MOUSE_BUTTON_4, QK_MOUSE_BUTTON_5, HK_RESET),
+ 
+LAYOUT(
+    KC_LEFT_SHIFT, HK_P_SET_D, HK_P_SET_BUF,
+    QK_MOUSE_BUTTON_4, QK_MOUSE_BUTTON_5, HK_DUMP),
 
 };
 
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS_LEFT][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+    [1] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+    [2] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+};
+#endif
 
 bool initialized = 0;
 
--- keyboards/rosmo/readme.md
+++ keyboards/rosmo/readme.md
@@ -1,9 +1,8 @@
 # RP2040 keyboard with Trackpoint and three buttons
 
-To trigger keypress, short together pins *GP4* and *GP5*.
 
-Double-tap reset to enter bootloader mode. Copy the built uf2 file to the device by dragging the file to the new USB disk.
 
 ## Supported Hardware
 
 * [Holykeebs Trackpoint module]()
+* EVQWGD001 scroll wheel encoder
